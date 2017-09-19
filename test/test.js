a72cdab06b470e03b7a66ac3ae76e098ec1bdfd2
SharksevenRo <937257166@qq.com>
2017-09-19 16:00:54 +0800

    [update] support filter config and page search

--- test/test.js
+++ test/test.js
@@ -5,8 +5,8 @@ const CloudSearch = opensearch.CloudSearch;
 
 const client_opts = {
     host: 'http://opensearch-cn-qingdao.aliyuncs.com',
-    accessKeyId: 'xxx',
-    secret: 'xxxx',
+    accessKeyId: 'LTAI3hadfiygtVRH',
+    secret: 'UBFWT8rtzLeDRlEHSOA0AgkAsjzWjn',
     debug: true
 };
 
@@ -14,8 +14,12 @@ const client = new CloudClient(client_opts);
 
 const search = new CloudSearch(client);
 search.addIndex('sharkseven');
-search.setQueryString('default:搜索');
-// search.setFormat('json');
+search.setQueryString('default:计算');
+search.setFormat('json');
+search.addFilter('feature=' + 2);
+search.setStartHit(0);
+search.setHits(2);
+search.setFirstFormulaName('feature_order_by');
 console.log(search);
 search.search().then(data => {
     console.log(data);
